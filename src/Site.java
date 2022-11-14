d0c17468270ae04794c57cae28902fd3370a1ada
josh1 <joshuanoronha3@cmail.carleton.ca>
2022-11-14 16:10:34 -0500

    Added getBaseAmount() and getTaxAmount() to Site

--- src/Site.java
+++ src/Site.java
@@ -1,5 +1,15 @@
 public class Site {
+
     public double getBillableAmount(){
         return getBaseAmount() + getTaxAmount();
     }
+    
+    public double getBaseAmount(){
+        return getBaseAmount();
+    }
+
+    protected double getTaxAmount(){
+        return getTaxAmount();
+    }
+
 }
