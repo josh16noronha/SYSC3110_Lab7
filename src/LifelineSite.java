505cc3741aab24e7cb7671fe121133d531ae965b
josh1 <joshuanoronha3@cmail.carleton.ca>
2022-11-14 15:33:09 -0500

    Initial Version

---
+++ src/LifelineSite.java
@@ -0,0 +1,13 @@
+public class LifelineSite extends Site{
+
+    private double _units;
+    private double _rate;
+
+
+    public double getBillableAmount(){
+        double base = _units * _rate * 0.5;
+        double tax = base * Site.TAX_RATE * 0.2;
+        return base + tax;
+
+    }
+}
