76fedde3899c4eac53685e357bd263df2338682c
josh1 <joshuanoronha3@cmail.carleton.ca>
2022-11-14 15:52:31 -0500

    Safe deleated getBillableAmount() from children classes

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -4,10 +4,4 @@ public class LifelineSite extends Site{
     private double _rate;
 
 
-    public double getBillableAmount(){
-        double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE * 0.2;
-        return base + tax;
-
-    }
 }
--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -3,12 +3,4 @@ public class ResidentialSite extends Site{
     private double _rate;
 
 
-    public double getBillableAmount(){
-        double base = _units * _rate;
-        double tax = base * Site.TAX_RATE;
-        return base + tax;
-
-    }
-
-
 }
