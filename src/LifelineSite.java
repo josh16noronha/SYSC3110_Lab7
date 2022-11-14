29aed1efa8d973140d7cc483e6786f9e1fc41672
josh1 <joshuanoronha3@cmail.carleton.ca>
2022-11-14 16:02:29 -0500

    Added getBaseAmount() and getTaxAmount() to LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -2,6 +2,19 @@ public class LifelineSite extends Site{
 
     private double _units;
     private double _rate;
+    private double base;
+    private double tax;
+
+    public double getBaseAmount(){
+        base = _units * _rate * 0.5;
+        return base;
+
+    }
+    public double getTaxAmount(){
+        tax = base * Site.TAX_RATE * 0.2;
+        return tax;
+
+    }
 
 
 }
